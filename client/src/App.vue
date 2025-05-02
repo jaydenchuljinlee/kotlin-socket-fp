5bbaf1a0272b25775944ea016a0a72e3b1f57ee2
ironjin <cheoljin1234@naver.com>
2025-05-02 17:05:15 +0900

    Added: Client Side 코드 반영

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Chat from './components/Chat.vue'
+</script>
+
+<template>
+  <main class="min-h-screen w-full">
+    <Chat />
+  </main>
+</template>
+
+<style>
+/* 전역 스타일 */
+body {
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+  width: 100%;
+  height: 100vh;
+}
+
+main {
+  height: 100%;
+  width: 100%;
+}
+</style>
